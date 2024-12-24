86e7b1f8e74d9fe981207662791226ca83fd98ba
HP-1e <3081925122@qq.com>
2024-12-24 12:25:39 +0800

    Update jinhua.js

--- kubejs/server_scripts/hpdlc_peifang/jinhua.js
+++ kubejs/server_scripts/hpdlc_peifang/jinhua.js
@@ -1,5 +1,1 @@
- //食物鱼缸
- const hpdlcorganCharmNbtMap = {
-    'hpdlc:glass_jar': { type: 'diet', dietTask: { minHunger: 4, foodTypeAmount: 30, foodTypeList: [] }, targetOrgan: 'hpdlc:fish_in_food' },
-}
-var assign_charm_recipes = Object.assign(organCharmNbtMap, hpdlcorganCharmNbtMap)
+
